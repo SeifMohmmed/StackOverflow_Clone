c3803b55c11273ec6021d054f6b6a24696e3f83b
Seif Mohamed <seif.eissa7797@gmail.com>
2026-03-12 18:41:31 +0200

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const Home = () => {
   return (
     <>
-      <h1 className="text-3xl text-violet-500">Welcome To Next.JS</h1>
+      <div>
+        <h1 className="h1-bold">Tailwind CSS is FUN !</h1>
+      </div>
     </>
   );
 };
